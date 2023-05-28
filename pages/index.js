8bae81875a979c58eee18a19daf50e2ca996d6a7
Austine Gomez <austine.gomez@yahoo.com>
2023-05-27 20:43:46 -0500

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,84 @@
 import { useState } from 'react';
-import Head from 'next/head';
 import Image from 'next/image';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import starImage from '../images/icon-star.svg';
 import thanks from '../images/illustration-thank-you.svg';
-import ThankYou from '../components/ThankYou';
-import styles from '../styles/Home.module.css';
-export default function Home() {
+
+const ratings = [
+  { id: 1, value: '1' },
+  { id: 2, value: '2' },
+  { id: 3, value: '3' },
+  { id: 4, value: '4' },
+  { id: 5, value: '5' },
+];
+
+const ThankYouMessage = ({ selectedRating }) => (
+  <div>
+    <main className={styles.main}>
+      <div className={styles.grid}>
+        <div className={styles.thankcard}>
+          <Image src={thanks} alt="thank you" width={100} height={90} />
+          <div className={styles.thanktext}>
+            <h4 className={styles.thanktextp}>You selected {selectedRating.value} out of 5</h4>
+          </div>
+          <h3>Thank you!</h3>
+          <p>
+            We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in
+          </p>
+        </div>
+      </div>
+    </main>
+  </div>
+);
+
+const Home = () => {
   const [isSubmitted, setSubmitted] = useState(false);
-  const ratings = [
-    { id: 1, value: '1' },
-    { id: 2, value: '2' },
-    { id: 3, value: '3' },
-    { id: 4, value: '4' },
-    { id: 5, value: '5' },
-  ];
+  const [selectedRating, setSelectedRating] = useState(ratings[0]);
 
-
-
-
- const [rating, setRating] = useState(ratings[0]);
- const ratingBtnClickHandler = (event) => {
-     setRating(ratings.find((rating) => rating.id === +event.target.textContent));
-   }
-   console.log(rating);
+  const handleRatingButtonClick = (event) => {
+    setSelectedRating(ratings.find((rating) => rating.id === +event.target.textContent));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Perform your form submission logic here
     console.log('Form submitted');
-    // After the form is submitted, set the isSubmitted state to true
     setSubmitted(true);
   };
 
-
-
-  const thankYou = () => {
-    return (
-      <div>
-        <main className={styles.main}>
-          <div className={styles.grid}>
-            <div className={styles.card}>
-          <Image src={thanks} alt="thank you" width={300} height={300} />
-          You selected {rating.value} rating.out of 5
-              <h3>Thank you!</h3>
-              <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in</p>
-            </div>
-          </div>
-        </main>
-      </div>
-    );
-  }
-
-  if (isSubmitted) {
-   return thankYou();
-  }
-  // Render your form he
-  
   return (
     <div className={styles.container}>
       <Head>
         <title>Frontend Mentor | Interactive Rating Component</title>
         <link rel="icon" href="../images/favicon-32x32.png" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <div className={styles.stardiv}>
-              <Image src={starImage} width={20} height={20} alt="starimage" className={styles.starimage} />
-            </div>
-            <h3>How did we do?</h3>
-            <p> Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering! .</p>
-            <div className={styles.rating}>
-              {ratings.map((rating) => (
-                <button key={rating.id} className={styles.ratingbtn} onClick={ratingBtnClickHandler}>
-                  {rating.id}
-                </button>
-              ))}
-            </div>
+      {isSubmitted ? (
+        <ThankYouMessage selectedRating={selectedRating} />
+      ) : (
+        <main className={styles.main}>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <div className={styles.stardiv}>
+                <Image src={starImage} width={20} height={20} alt="starimage" className={styles.starimage} />
+              </div>
+              <h3>How did we do?</h3>
+              <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+              <div className={styles.rating}>
+                {ratings.map((rating) => (
+                  <button key={rating.id} className={styles.ratingbtn} onClick={handleRatingButtonClick}>
+                    {rating.value}
+                  </button>
+                ))}
+              </div>
               <form onSubmit={handleSubmit}>
-                 <button className={styles.submitbtn} >Submit</button>
+                <button className={styles.submitbtn}>Submit</button>
               </form>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      )}
     </div>
   );
-}
+};
+
+export default Home;
